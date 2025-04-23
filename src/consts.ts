1bb6015bad7448eaf32382d2f43210ddd332f5ce
Michael Warf <michael@coalbanks.com>
2025-04-23 17:46:47 -0600

    fixed site title and meta description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "BrainMuffin | Hollywood Magic from the Other L.A.";
+export const SITE_DESCRIPTION = "We combine AI power with human creativity to deliver stunning visual effects fast and affordably.";
